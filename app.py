5df2f63ddfefe2cf24176d18cd625e460ac45892
sjylawyer <sjylawyer@gmail.com>
2019-01-16 21:07:08 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -56,11 +56,23 @@ def forge():
     
     db.session.commit()
     click.echo('Done.')
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    user=User.query.first()
+    return render_template('404.html'),404
+
 @app.route('/')
 def index():
-    user= User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
+
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: {}'.format(name)
@@ -77,3 +89,6 @@ def test_url_for():
     # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
+
+
+
